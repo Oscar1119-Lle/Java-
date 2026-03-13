95cc3f40d7115369ccb83a7f94a5222f291abb88
Oscar1119-Lle <2799639821@qq.com>
2026-03-13 13:06:16 +0800

    commit

--- src/Feedback.java
+++ src/Feedback.java
@@ -23,31 +23,28 @@ public class Feedback {
     }
 
     private String feedbackUsingConcatenation(String sent1, String sent2, String sent3, String sent4, String sent5) {
-        String concatenatedFeedback = sent1 + sent2 + sent3 + sent4 + sent5;
+        String concatenatedFeedback = sent1+ " " + sent2 + " " + sent3 + " " + sent4 + " " + sent5;
         return concatenatedFeedback;
     }
 
     private StringBuilder feedbackUsingStringBuilder(String sent1, String sent2, String sent3, String sent4, String sent5) {
         StringBuilder sb = new StringBuilder();
-        sb.append(sent1);
-        sb.append(sent2);
-        sb.append(sent3);
-        sb.append(sent4);
-        sb.append(sent5);
+        sb.append(sent1).append(" ");sb.append(sent2).append(" ");sb.append(sent3).append(" ");sb.append(sent4).append(" ");sb.append(sent5).append(" ");
         return sb;
     }
 
     private boolean checkFeedbackLength(String completeFeedback) {
-        this.longFeedback = completeFeedback.length() > 500;
-        return this.longFeedback;
+        longFeedback = completeFeedback.length() > 500;
+        return longFeedback;
     }
 
     private void createReviewID(String firstName, String lastName, String completeFeedback) {
-        String namePart = (firstName + lastName).substring(2, 6).toUpperCase();
-        String feedbackPart = completeFeedback.substring(10, 15).toLowerCase();
-        int length = completeFeedback.length();
-        long timestamp = System.currentTimeMillis();
-        this.reviewID = (namePart + feedbackPart + length + "_" + timestamp).replace(" ", "");
+        reviewID= (firstName + lastName).substring(2, 6).toUpperCase();
+        reviewID+= completeFeedback.substring(10, 15).toLowerCase();
+        reviewID+= completeFeedback.length();
+        reviewID+="_";
+        reviewID+= System.currentTimeMillis();
+        reviewID =reviewID.replace(" ", "");
     }
 
     public void analyseFeedback(boolean isConcatenation, String sent1, String sent2, String sent3, String sent4, String sent5) {
@@ -56,8 +53,7 @@ public class Feedback {
             checkFeedbackLength(completeFeedback);
             createReviewID(firstName, lastName, completeFeedback);
         } else {
-            StringBuilder sb = feedbackUsingStringBuilder(sent1, sent2, sent3, sent4, sent5);
-            completeFeedback = sb.toString();
+            completeFeedback = feedbackUsingStringBuilder(sent1, sent2, sent3, sent4, sent5).toString();
             checkFeedbackLength(completeFeedback);
             createReviewID(firstName, lastName, completeFeedback);
         }
@@ -65,13 +61,6 @@ public class Feedback {
 
     @Override
     public String toString() {
-        return "Feedback{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", completeFeedback='" + completeFeedback + '\'' +
-                ", longFeedback=" + longFeedback +
-                ", reviewID='" + reviewID + '\'' +
-                '}';
+        return "Feedback{" + "firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", email='" + email + '\'' + ", completeFeedback='" + completeFeedback + '\'' + ", longFeedback=" + longFeedback + ", reviewID='" + reviewID + '\'' + '}';
     }
 }
